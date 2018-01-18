22df57b99b03b1839d132b7efd830f02ddcab3e6
Luke Mwila <outlier.developer@gmail.com>
2018-01-18 22:52:43 +0200

    Added functionality for adding ingredients to the burger.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,7 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map((ctrl) => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl key={ctrl.label} label={ctrl.label} type={ctrl.type} added={() => props.ingredientAdded(ctrl.type)}  />
         ))}
     </div>
 )
